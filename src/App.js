c7edae8659b4ad8494eec04743fcbed075902b73
lucho20091 <124323772+lucho20091@users.noreply.github.com>
2024-06-13 15:39:48 -0500

    commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 import './App.css';
 import Home from './components/Home'
 import Quiz from './components/Quiz'
-import { useState, useEffect, useRef } from 'react'
-
-
+import { useState, useEffect } from 'react'
 
 function App() {
   const [home, setHome] = useState(true)
